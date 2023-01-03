463a4882633306722d14e94d2028ffc5bfb49f2d
NMDRA <nimendradilshan11@gmail.com>
2023-01-03 08:45:54 +0530

    Add new autocmd

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -74,3 +74,13 @@ autocmd('BufLeave', {
   command = 'stopinsert'
 })
 
+--automatically jump to the last place you’ve visited in a file before exit
+autocmd('BufReadPost', {
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+})
