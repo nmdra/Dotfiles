2e7eba36eecb0fb04a853dddb52f4d4a10ce0bc2
nmdra <nimendradilshan11@gmail.com>
2024-01-19 20:07:20 +0530

    Update Config for VSCode Neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,19 +3,28 @@ This Configuration Based on Neovim-Lua by brainfucksec,
 Website: https://github.com/brainfucksec/neovim-lua
 --]]
 
--- Import Lua modules
 require('core/lazy')
-require('core/autocmds')
-require('core/keymaps')
-require('plugins/lualine') -- statusline config
-require('core/options')
-require('lsp/lspconfig')
-require('plugins/nvim-tree')
-require('plugins/nvim-cmp')
-require('plugins/nvim-treesitter')
-require('plugins/comment') --comment plugin
-require('plugins/tokyonight')
-vim.cmd[[colorscheme tokyonight]]
 
-require('plugins/indent-blankline')
-require('plugins/toggleterm')
+if vim.g.vscode then
+  -- VSCode extension
+  require('core/options')
+  require('core/autocmds')
+  require('core/keymaps')
+else
+  -- ordinary Neovim
+  -- Import Lua modules
+  require('core/autocmds')
+  require('core/keymaps')
+  require('plugins/lualine') -- statusline config
+  require('core/options')
+  require('lsp/lspconfig')
+  require('plugins/nvim-tree')
+  require('plugins/nvim-cmp')
+  require('plugins/nvim-treesitter')
+  require('plugins/comment') --comment plugin
+  require('plugins/tokyonight')
+  vim.cmd[[colorscheme tokyonight]]
+
+  require('plugins/indent-blankline')
+  require('plugins/toggleterm')
+end
