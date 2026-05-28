513a3306ac6a58643761c4e3525d303a74144c6c
nmdra <nimendradilshan11@gmail.com>
2026-05-28 07:22:21 +0530

    chore(nvim): update obsidian plugin config and add lazyvim lockfile

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -1,30 +1,15 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*", -- recommended, use latest release instead of latest commit
-  lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
-  dependencies = {
-    -- Required.
-    "nvim-lua/plenary.nvim",
-
-    -- see below for full list of optional dependencies 👇
-  },
+  "obsidian-nvim/obsidian.nvim",
+  version = "*", -- use latest release, remove to use latest commit
+  ---@module 'obsidian'
+  ---@type obsidian.config
   opts = {
+    legacy_commands = false, -- this will be removed in 4.0.0
     workspaces = {
       {
         name = "personal",
         path = "~/Documents/Second Brain 2.0/",
       },
     },
-
-    -- see below for full list of options 👇
   },
 }
